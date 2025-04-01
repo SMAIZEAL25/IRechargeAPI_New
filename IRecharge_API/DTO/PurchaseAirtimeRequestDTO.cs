500045ef35288170d07528ffc10b1dede82f97c0
smaizeal25 <samizeal25@gmail.com>
2025-04-01 14:09:01 +0100

    modification

--- IRecharge_API/DTO/PurchaseAirtimeRequestDTO.cs
+++ IRecharge_API/DTO/PurchaseAirtimeRequestDTO.cs
@@ -5,16 +5,11 @@ namespace IRecharge_API.DTO
     public class PurchaseAirtimeRequestDTO
     {
         [Required(ErrorMessage = "Phone number is required")]
-        [RegularExpression(@"^0[7-9][0-9]{9}$", ErrorMessage = "Invalid Nigerian phone number")]
         public required string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Amount is required")]
-        [Range(50, 10000, ErrorMessage = "Amount must be between ₦50 and ₦10,000")]
         public decimal Amount { get; set; }
 
-        [Required(ErrorMessage = "Network type is required")]
-        [RegularExpression("^(MTN|AIRTEL|GLO|9MOBILE)$",
-        ErrorMessage = "Network must be MTN, AIRTEL, GLO, or 9MOBILE")]
         public required string NetworkType { get; set; }
     }
 }
